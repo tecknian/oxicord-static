a7d073f57f6024c5daa1c0352a26bc05230ccd7d
Braian A. Diez <bdiez19@gmail.com>
2026-01-26 10:37:30 -0300

    chore: release v0.1.7

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       in {
         packages.default = rustPlatform.buildRustPackage {
           pname = "oxicord";
-          version = "0.1.6";
+          version = "0.1.7";
 
           src = ./.;
 
